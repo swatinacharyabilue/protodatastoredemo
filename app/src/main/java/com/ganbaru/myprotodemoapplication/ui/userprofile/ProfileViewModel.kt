7ad6af97744ac79c195a0ad8b436c8dd1855a525
swatinacharyabilue <139827615+swatinacharyabilue@users.noreply.github.com>
2024-02-08 09:55:43 +1100

    update gradle
    update gradle

--- app/src/main/java/com/ganbaru/myprotodemoapplication/ui/userprofile/ProfileViewModel.kt
+++ app/src/main/java/com/ganbaru/myprotodemoapplication/ui/userprofile/ProfileViewModel.kt
@@ -1,7 +1,6 @@
 package com.ganbaru.myprotodemoapplication.ui.userprofile
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ganbaru.myprotodemoapplication.UserProfile
